a3d142eea42992b6f7a8bd3d3dd101f42e36771d
Aiden Vigue <acvigue@me.com>
2021-04-29 17:15:28 -0400

    Initial Commit

---
+++ JellyfinPlayer/JellyfinPlayerApp.swift
@@ -0,0 +1,20 @@
+//
+//  JellyfinPlayerApp.swift
+//  JellyfinPlayer
+//
+//  Created by Aiden Vigue on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct JellyfinPlayerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
